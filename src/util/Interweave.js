554e1630e6d3627ea852e82a31af0589603af29f
arham-lodha <55772347+arham-lodha0317@users.noreply.github.com>
2021-06-24 15:58:24 -0500

    Fixed Images

--- src/util/Interweave.js
+++ src/util/Interweave.js
@@ -1,4 +1,4 @@
-import { Center, Heading, Link, Text } from "@chakra-ui/react";
+import { AspectRatio, Center, Heading, Image, Link, Text } from "@chakra-ui/react";
 
 export function transform(node, children){
     if(node.tagName.toLowerCase() === "h1"){       
@@ -15,7 +15,17 @@ export function transform(node, children){
         </Center>
       );
     }
-    else if(node.tageName.toLowerCase === "a"){
+    else if(node.tagName.toLowerCase() === "a"){
+      const regex = RegExp("(jpg|gif|png)$");
+
+      if(regex.test(node.getAttribute("href"))){
+        return (
+          <Center m={4} maxW="sm">
+            <Image src={node.getAttribute("href")} />
+          </Center>
+        );
+      }
+      
       return <Link href={node.getAttribute('href')} color="teal">{children}</Link>
     }
     else if(node.tagName.toLowerCase() === "p"){
